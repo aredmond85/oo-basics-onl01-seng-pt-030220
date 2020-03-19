a5bc64cded59c7bbad80f94989f8f8fc72ea8b30
Austin Redmond <aredmond85@gmail.com>
2020-03-19 01:38:20 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 
 class Shoe
 
-  attr_accessor :brand, :color, :size, :material, :condition
+attr_accessor :brand, :color, :size, :material, :condition
 
   def initialize(brand)
     @brand = brand
